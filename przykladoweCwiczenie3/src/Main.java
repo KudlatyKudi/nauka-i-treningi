854cf68e7dcccbc1942e1869409f40cc9a79694b
KudlatyKudi <110770611+KudlatyKudi@users.noreply.github.com>
2022-08-07 18:18:40 +0200

    pierwsze kroki 2

--- przykladoweCwiczenie3/src/Main.java
+++ przykladoweCwiczenie3/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+
+       printMegaBytesAndKiloBytes(6008);
+
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int remainingKilobytes = kiloBytes % 1024;
+            int megaBytes = kiloBytes / 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes +
+                    " MB and " + remainingKilobytes +
+                    " KB");
+        }
     }
 }
